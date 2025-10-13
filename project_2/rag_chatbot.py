9da1caea3615d8789cc4d4a812f1bab304774d8e
Liamarcia <liamarciavbifano@gmail.com>
2025-10-13 08:41:15 +0100

    project 2 session 2

--- project_2/rag_chatbot.py
+++ project_2/rag_chatbot.py
@@ -24,5 +24,39 @@ from langchain.prompts import PromptTemplate
 
 print("✅ Libraries imported! You're good to go!")
 
+
+def ingest_pdfs():
+    pdf_paths = glob.glob("data/Everstorm_*.pdf")
+    raw_docs = [PyPDFLoader(path).load() for path in pdf_paths]
+
+    return raw_docs
+
+    # print(f"Loaded {len(raw_docs)} PDF pages from {len(pdf_paths)} files.")
+
+
+def ingest_urls():
+    URLS = [
+    # --- BigCommerce – shipping & refunds ---
+    "https://developer.bigcommerce.com/docs/store-operations/shipping",
+    "https://developer.bigcommerce.com/docs/store-operations/orders/refunds",
+    # --- Stripe – disputes & chargebacks ---
+    # "https://docs.stripe.com/disputes",  
+    # --- WooCommerce – REST API reference ---
+    # "https://woocommerce.github.io/woocommerce-rest-api-docs/v3.html",
+    ]
+    return UnstructuredURLLoader(urls=URLS).load()
+    
+
 if __name__ == '__main__':
     print("oi")
+
+    ingested_pdfs = ingest_pdfs()
+    ingested_urls = ingest_urls()
+
+    docs = ingested_pdfs + ingested_urls
+
+    chunked_docs = [RecursiveCharacterTextSplitter(doc) for doc in docs]
+
+    print(len(chunked_docs[0]))
+
+
