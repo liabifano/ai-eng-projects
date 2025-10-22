3172b3b998f5a5ff986f1dd9507a6f1ccb017f91
Liamarcia <liamarciavbifano@gmail.com>
2025-10-22 16:58:50 +0100

    duckduckgo

--- project_3/ask_the_web_agent.py
+++ project_3/ask_the_web_agent.py
@@ -4,7 +4,7 @@ import inspect
 from langchain_core.tools import tool
 from langchain_community.chat_models import ChatOllama
 from langchain.agents import initialize_agent, Tool, AgentType
-import pprint
+from duckduckgo_search import DDGS
 
 client = OpenAI(api_key = "ollama", base_url = "http://localhost:11434/v1")
 
@@ -21,10 +21,10 @@ def get_current_weather(city: str, unit: str = "celsius") -> str:
     complete_url = f"{base_url}&appid={OPENWEATHERMAP_API_KEY}&?q={city}"
     response = requests.get(complete_url).json()
     # return response
-    return "It's 13C and a lot of rain"
+    return "It will be sunny, 23C, won't rain"
 
 @tool
-def get_weather(city) -> str:
+def get_weather(city: str) -> str:
     """
     Retuns weather forecast from openweathermap.org
     """
@@ -37,7 +37,15 @@ def select_and_call_webtools(USER_QUERY: str) -> dict:
         return {"get_current_wheater": get_current_weather(city)}
     
     else: return {}
-    
+
+@tool
+def web_search(query: str) -> str:
+    """
+    Search in the web using duckduckgo
+    """
+    results = DDGS().text(query, max_results=5)
+    return "\n\n".join([f"{r['title']} - ref:{r['href']} - {r["body"]}" for r in results])
+
 
 def to_schema(fn):
     return {"name": fn.name,
@@ -50,22 +58,23 @@ def to_schema(fn):
 
 if __name__ == '__main__':
     city = "San Francisco"
-    USER_QUERY = f"""Should I bring umbrella todat in {city}?"""
+    USER_QUERY = f"""What is the weather in SF today?"""
 
-    TOOLS_SCHEMA = {"get_weather": to_schema(get_weather)}
+    TOOLS_SCHEMA = {"get_weather": to_schema(get_weather), "web_search": to_schema(web_search)}
     tools = [Tool(**t) for t in TOOLS_SCHEMA.values()]
 
     SYSTEM_PROMPT = f"""
     User asked: {USER_QUERY}
-    Here is the tools you can choose from and execute to answer user's question: 
+    Here is the tools you can choose from and execute to answer user's question. 
         {TOOLS_SCHEMA}
 
     Do the steps in the following order: 
-        1) In TOOLS_SCHEMA, find a tools that matches or answers user's query
-        2) Map the "args" of the matched tools in step 1. Use default if user didn't specify.
-        3) If any argument is missing and there is no default, ask to the user include it in the query
-        4) Execute the tools 
-        5) Reason the user's query and the returned values from the tools, provide an empatetic and clear answer to the final user
+        1) Always select web search in TOOLS_SCHEMA with the whole user query
+        2) In TOOLS_SCHEMA, find additional tools that matches or answers user's query. 
+        3) Map the "args" of the matched tools in step 1. Use default if user didn't specify.
+        4) If any argument is missing and there is no default, ask to the user include it in the query
+        5) Execute the tools 
+        6) Summarize tools results, reason the user's query and the returned values from the tools, provide an empatetic and clear answer to the final user
     """
 
     model = ChatOllama(model="gemma3:1b", temperature=0)
@@ -75,21 +84,3 @@ if __name__ == '__main__':
     output = agent.invoke(SYSTEM_PROMPT, verbose=True)['output']
 
     print(output)
-
-
-  #stream = client.chat.completions.create(
-  #    model="mistral:7b",
-  #    messages=[{
-  #        "role": "system",
-  #        "content": "You are a helpful assistant that uses retrieved webdata to answer questions"
-  #        }, {
-  #            "role": "user",
-  #            "content": SYSTEM_PROMPT
-  #        }, 
-  #        ],
-  #    stream=True,
-  #)
-
-  #for chunk in stream:
-  #    print(chunk.choices[0].delta.content or "", end="")
-  #
