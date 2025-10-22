da89ee886a1d2f9e3bed98b99bc109680dc2b15a
Liamarcia <liamarciavbifano@gmail.com>
2025-10-22 13:46:12 +0100

    ex 3 week 3

--- project_3/ask_the_web_agent.py
+++ project_3/ask_the_web_agent.py
@@ -1,12 +1,19 @@
 from openai import OpenAI
 import requests
+import inspect
 
 client = OpenAI(api_key = "ollama", base_url = "http://localhost:11434/v1")
 
 OPENWEATHERMAP_API_KEY = ""
 
 
-def get_current_wheater(city: str, unit: str = "celsius") -> str:
+def get_current_weather(city: str, unit: str = "celsius") -> str:
+    """ Retuns weather forecast from openweathermap.org
+
+    @city: str City
+    @unit: str Temperature Unit (default celsius)
+    """
+
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     complete_url = f"{base_url}&appid={OPENWEATHERMAP_API_KEY}&?q={city}"
     response = requests.get(complete_url).json()
@@ -17,30 +24,39 @@ def get_current_wheater(city: str, unit: str = "celsius") -> str:
 def select_and_call_webtools(USER_QUERY: str) -> dict:
     if USER_QUERY.find('is the weather'):
         city = " ".join([x for x in USER_QUERY.split(" ") if x.istitle()][1:])
-        return {"get_current_wheater": get_current_wheater(city)}
+        return {"get_current_wheater": get_current_weather(city)}
     
     else: return {}
     
+
+TOOLS = {"weather": get_current_weather}
+
+def to_schema(fn):
+    return {"name": fn.__name__,
+            "description": fn.__doc__,
+            "args": list(inspect.signature(fn).parameters.keys()),
+            "types": [p.annotation for p in list(inspect.signature(fn).parameters.values())]
+            }
+
 
 if __name__ == '__main__':
     city = "San Francisco"
     USER_QUERY = f"""What is the weather in {city} today?"""
 
-    web_response = select_and_call_webtools(USER_QUERY)
+    TOOLS_SCHEMA = {"get_current_weather": to_schema(get_current_weather)}
 
     SYSTEM_PROMPT = f"""
     User asked: {USER_QUERY}
-    Here is the live info from the web: 
-        {web_response}
-        
-    Provide a helpful, clear answer.
+    Here is the tools you can choose from and execute to answer user's question: 
+        {TOOLS_SCHEMA}
 
-    Rules:
-        1) Check if the city exists and there is no misspelling. If the checks do not pass, ask the user to check its inputs and re-try
-        2) Be concise and accurate. Prefer quoting key phrases from the context.
-        3) When possible and trustable, cite sources as using the metadata
-
-    If the answer is not in CONTEXT or updated, respond with “I'm not sure from the docs.
+    Do the steps in the following order: 
+        1) In TOOLS_SCHEMA, find a tools that matches or answers user's query
+        2) Map the "args" of the matched tools in step 1
+        3) If any argument is missing and there is no default, ask to the user include it in the query
+        4) Provide which tools and arguments its required to answer user's query. 
+            The output must be a list of dictionaries where key are tools' names and values are dictionaries
+            where keys are args names and values are args values. 
     """
 
     stream = client.chat.completions.create(
